05d91d6b56f5a3a4af72407327e6d213a1370349
Valentín Tur <valentin.rj.tur@gmail.com>
2023-03-10 19:52:49 -0300

    agregando la cantidadTotal al codigo js

--- js/main.js
+++ js/main.js
@@ -2,15 +2,22 @@ let precioRemeras = 2500
 let precioBuzos = 4000
 let precioPantalones = 3000
 let precioZapatillas = 6000
+let cantidadTotal = 0
+let total = 0
 
-function precio_Total(cantidad, prenda) {
-
+function montoPrendas(cantidad, prenda) {
         let subtotal = cantidad * prenda
-        alert(`El total a pagar es de ${subtotal} por ${cantidad} prendas.`) 
+        total += subtotal;
+        alert(`El monto de esta compra es de ${subtotal} por ${cantidad} prendas.`) 
+} 
+
+function precioTotal(){
+    alert(`Gracias por elegirnos! El precio total de la compra es de $${total} por ${cantidadTotal} prendas`)
 }
 
 let nombre = prompt("Ingrese su nombre")
 alert(`Bienvenido/a ${nombre}! ¿Qué desea comprar?`);
+
 
 let numero = parseInt(prompt(`Presione el número según su elección.
 [1] Remeras
@@ -22,7 +29,8 @@ let numero = parseInt(prompt(`Presione el número según su elección.
 while(numero != 5) {
                 if(numero == 1){                        
                      cantidad = parseInt(prompt("El precio de las remeras es de $2500. ¿Cuántas desea comprar?"))
-                     precio_Total(cantidad, precioRemeras)
+                     montoPrendas(cantidad, precioRemeras)
+                     cantidadTotal += cantidad
 
                      numero = parseInt(prompt(`Presione el número según su elección.
                      [1] Remeras
@@ -33,7 +41,8 @@ while(numero != 5) {
                 }
                 else if(numero == 2){
                     cantidad = parseInt(prompt("El precio de los buzos es de $4000. ¿Cuántos desea comprar?"))
-                    precio_Total(cantidad, precioBuzos)
+                    montoPrendas(cantidad, precioBuzos)
+                    cantidadTotal += cantidad
 
                     numero = parseInt(prompt(`Presione el número según su elección.
                     [1] Remeras
@@ -44,7 +53,8 @@ while(numero != 5) {
                }
                else if(numero == 3){
                    cantidad = parseInt(prompt("El precio de los pantalones es de $3000. ¿Cuántos desea comprar?"))
-                   precio_Total(cantidad, precioPantalones)
+                   montoPrendas(cantidad, precioPantalones)
+                   cantidadTotal += cantidad
 
                    numero = parseInt(prompt(`Presione el número según su elección.
                    [1] Remeras
@@ -55,7 +65,8 @@ while(numero != 5) {
                }
                 else if(numero == 4){
                    cantidad = parseInt(prompt("El precio de las zapatillas es de $6000. ¿Cuántas desea comprar?"))
-                   precio_Total(cantidad, precioZapatillas)
+                   montoPrendas(cantidad, precioZapatillas)
+                   cantidadTotal += cantidad
 
                    numero = parseInt(prompt(`Presione el número según su elección.
                    [1] Remeras
@@ -77,3 +88,4 @@ while(numero != 5) {
             }
 
 
+            precioTotal();
