d717f5fc7b8c32ebbae27f6a118c222bbb6d9f56
Valentín Tur <valentin.rj.tur@gmail.com>
2023-03-28 09:31:53 -0300

    modificacion de script

--- js/main.js
+++ js/main.js
@@ -1,44 +1,44 @@
 const prendas = [
-    {nombre: "remera", precio: 2500},
-    {nombre: "camisa", precio: 2800},
-    {nombre: "buzo", precio: 4000},
-    {nombre: "jean", precio: 4000},
-    {nombre: "pantalon", precio: 3000},
-    {nombre: "zapatilla", precio: 6000},
-    {nombre: "campera", precio: 5500},
+  { nombre: "remera", precio: 2500 },
+  { nombre: "camisa", precio: 2800 },
+  { nombre: "buzo", precio: 4000 },
+  { nombre: "jean", precio: 4000 },
+  { nombre: "pantalon", precio: 3000 },
+  { nombre: "zapatilla", precio: 6000 },
+  { nombre: "campera", precio: 5500 },
 ];
 
-let carrito = []
+let carrito = [];
 
 function seleccionCorrecta() {
-    while(seleccion != "si" && seleccion != "no"){
-        alert("Ingrese alguna de las dos opciones validas ( si ) o ( no )")
-        seleccion = prompt("¿Desea hacer alguna compra? ( si ) o ( no )")
-    }
-
+  while (seleccion != "si" && seleccion != "no") {
+    alert("Ingrese alguna de las dos opciones validas ( si ) o ( no )");
+    seleccion = prompt("¿Desea hacer alguna compra? ( si ) o ( no )");
+  }
 }
 
-let nombre = prompt("Ingrese su nombre")
+let nombre = prompt("Ingrese su nombre");
 alert(`Bienvenido/a ${nombre}!`);
 
-let seleccion = prompt("¿Desea hacer alguna compra? Conteste con ( si ) o con ( no )")
+let seleccion = prompt(
+  "¿Desea hacer alguna compra? Conteste con ( si ) o con ( no )"
+);
 
-seleccionCorrecta()
+seleccionCorrecta();
 
+if (seleccion == "si") {
+  alert("A continuación nuestra indumentaria...");
+  let todasLasPrendas = prendas.map(
+    (prenda) => prenda.nombre + " " + "$" + prenda.precio
+  );
 
-if(seleccion == "si"){
-    alert("A continuación nuestra indumentaria...")
-    let todasLasPrendas = prendas.map(
-        (prenda) => prenda.nombre + " " + "$" + prenda.precio
-        );
-
-    alert(todasLasPrendas.join(" / "))
-} else if (seleccion == "no"){
-    alert("Gracias por visitarnos, hasta pronto!");
+  alert(todasLasPrendas.join(" / "));
+} else if (seleccion == "no") {
+  alert("Gracias por visitarnos, hasta pronto!");
 }
 
-while(seleccion != "no") {
-    let prenda = prompt(`Agregá una prenda a tu carrito
+while (seleccion != "no") {
+  let prenda = prompt(`Agregá una prenda a tu carrito
     -remera-
     -camisa-
     -buzo-
@@ -46,59 +46,69 @@ while(seleccion != "no") {
     -pantalon-
     -zapatilla-
     -campera-
-    `)
-    let precio = 0
+    `);
+  let precio = 0;
 
-    if(prenda == "remera" || prenda == "camisa" || prenda == "buzo" || prenda == "jean" || prenda == "pantalon" || prenda == "zapatilla" || prenda == "campera") {
-         switch(prenda){
-            case "remera": 
-            precio = 2500
-            break;
-            case "camisa": 
-            precio = 2800
-            break;
-            case "buzo": 
-            precio = 4000
-            break;
-            case "jean": 
-            precio = 4000
-            break;
-            case "pantalon": 
-            precio = 3000
-            break;
-            case "zapatilla": 
-            precio = 6000
-            break;
-            case "campera": 
-            precio = 5500
-            break;
-            default:
-            break;
-         }
-         let unidades = parseInt(prompt(`¿Cuántas unidades desea llevar?`))
-
-         carrito.push({prenda, unidades, precio})
-
-    }  else{
-        alert(`No contamos con esa prenda en stock`)
+  if (
+    prenda == "remera" ||
+    prenda == "camisa" ||
+    prenda == "buzo" ||
+    prenda == "jean" ||
+    prenda == "pantalon" ||
+    prenda == "zapatilla" ||
+    prenda == "campera"
+  ) {
+    switch (prenda) {
+      case "remera":
+        precio = 2500;
+        break;
+      case "camisa":
+        precio = 2800;
+        break;
+      case "buzo":
+        precio = 4000;
+        break;
+      case "jean":
+        precio = 4000;
+        break;
+      case "pantalon":
+        precio = 3000;
+        break;
+      case "zapatilla":
+        precio = 6000;
+        break;
+      case "campera":
+        precio = 5500;
+        break;
+      default:
+        break;
     }
+    let unidades = parseInt(prompt(`¿Cuántas unidades desea llevar?`));
 
-    seleccion = prompt(`¿Desea seguir comprando? ( si ) o ( no )`)
+    carrito.push({ prenda, unidades, precio });
+  } else {
+    alert(`No contamos con esa prenda en stock`);
+  }
 
-    seleccionCorrecta()
-    
-    
-    
-    while (seleccion === "no") {
-        alert(`Gracias por la compra, Hasta pronto!`)
-        alert(`A continuación su carrito final...`)
-        carrito.forEach((carritoFinal) => {
-            alert(`Prenda: ${carritoFinal.prenda} / Unidades: ${carritoFinal.unidades} / Total a pagar por producto: $${carritoFinal.precio * carritoFinal.unidades}.`)
-        })
+  seleccion = prompt(`¿Desea seguir comprando? ( si ) o ( no )`);
+
+  seleccionCorrecta();
+
+  while (seleccion === "no") {
+    alert(`Gracias por la compra, Hasta pronto!`);
+    alert(`A continuación su carrito final...`);
+    carrito.forEach((carritoFinal) => {
+      alert(
+        `Prenda: ${carritoFinal.prenda} / Unidades: ${
+          carritoFinal.unidades
+        } / Total a pagar por producto: $${
+          carritoFinal.precio * carritoFinal.unidades
+        }.`
+      );
+    });
     break;
-    }
-
+  }
 }
 
-const total = carrito.reduce((ecc, el) => ecc + el.precio * el.unidades, 0)
-alert(`El total a pagar por su compra es: $${total}.`)
+const total = carrito.reduce((ecc, el) => ecc + el.precio * el.unidades, 0);
+alert(`El total a pagar por su compra es: $${total}.`);
