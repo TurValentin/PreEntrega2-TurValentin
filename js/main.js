9a09899d7160fac7057837679dd03b48e103a2c4
Valentín Tur <valentin.rj.tur@gmail.com>
2023-04-04 20:41:01 -0300

    modificamos las rutas de las imagenes en el array de productos

--- js/main.js
+++ js/main.js
@@ -2,7 +2,7 @@ const productos = [
     {
         id: "remera-1",
         titulo: "Remera 1",
-        imagen: "../../img/remeras/remera1.jpg" ,
+        imagen: "./img/remeras/remera1.jpg" ,
         precio: 3000 ,
         categoria : {
             nombre: "Remeras",
@@ -12,7 +12,7 @@ const productos = [
     {
         id: "remera-2",
         titulo: "Remera 2",
-        imagen: "../../img/remeras/remera2.jpg" ,
+        imagen: "./img/remeras/remera2.jpg" ,
         precio: 3000 ,
         categoria : {
             nombre: "Remeras",
@@ -22,7 +22,7 @@ const productos = [
     {
         id: "remera-3",
         titulo: "Remera 3 ",
-        imagen: "../../img/remeras/remera3.jpg" ,
+        imagen: "./img/remeras/remera3.jpg" ,
         precio: 3000 ,
         categoria : {
             nombre: "Remeras",
@@ -32,7 +32,7 @@ const productos = [
     {
         id: "remera-4",
         titulo: "Remera 4",
-        imagen: "../../img/remeras/remera4.jpg" ,
+        imagen: "./img/remeras/remera4.jpg" ,
         precio: 3000 ,
         categoria : {
             nombre: "Remeras",
@@ -42,7 +42,7 @@ const productos = [
     {
         id: "remera-5",
         titulo: "Remera 5",
-        imagen: "../../img/remeras/remera5.jpg" ,
+        imagen: "./img/remeras/remera5.jpg" ,
         precio: 2000 ,
         categoria : {
             nombre: "Remeras",
@@ -52,7 +52,7 @@ const productos = [
     {
         id: "remera-6",
         titulo: "Remera 6",
-        imagen: "../../img/remeras/remera6.jpg" ,
+        imagen: "./img/remeras/remera6.jpg" ,
         precio: 2500 ,
         categoria : {
             nombre: "Remeras",
@@ -62,7 +62,7 @@ const productos = [
     {
         id: "remera-7",
         titulo: "Remera 7",
-        imagen: "../../img/remeras/remera7.jpg" ,
+        imagen: "./img/remeras/remera7.jpg" ,
         precio: 2700 ,
         categoria : {
             nombre: "Remeras",
@@ -72,7 +72,7 @@ const productos = [
     {
         id: "remera-8",
         titulo: "Remera 8",
-        imagen: "../../img/remeras/remera8.jpg" ,
+        imagen: "./img/remeras/remera8.jpg" ,
         precio: 2500 ,
         categoria : {
             nombre: "Remeras",
@@ -82,7 +82,7 @@ const productos = [
     {
         id: "remera-9",
         titulo: "Remera 9",
-        imagen: "../../img/remeras/remera9.jpg" ,
+        imagen: "./img/remeras/remera9.jpg" ,
         precio: 2000 ,
         categoria : {
             nombre: "Remeras",
@@ -92,8 +92,8 @@ const productos = [
     {
         id: "remera-10",
         titulo: "Remera 10",
-        imagen: "../../img/remeras/remera10.jpg" ,
-        precio: 2000 ,
+        imagen: "./img/remeras/remera10.jpg" ,
+        precio: 2000,
         categoria : {
             nombre: "Remeras",
             id: "remeras"   
@@ -102,7 +102,7 @@ const productos = [
     {
         id: "pantalon-1",
         titulo: "Pantalón 1",
-        imagen: "../../img/pantalones/pantalon1.jpg" ,
+        imagen: "./img/pantalones/pantalon1.jpg" ,
         precio: 8000 ,
         categoria : {
             nombre: "Pantalones",
@@ -112,7 +112,7 @@ const productos = [
     {
         id: "pantalon-2",
         titulo: "Pantalón 2",
-        imagen: "../../img/pantalones/pantalon2.jpg" ,
+        imagen: "./img/pantalones/pantalon2.jpg" ,
         precio: 8000 ,
         categoria : {
             nombre: "Pantalones",
@@ -122,7 +122,7 @@ const productos = [
     {
         id: "pantalon-3",
         titulo: "Pantalón 3",
-        imagen: "../../img/pantalones/pantalon3.jpg" ,
+        imagen: "./img/pantalones/pantalon3.jpg" ,
         precio: 9500 ,
         categoria : {
             nombre: "Pantalones",
@@ -132,7 +132,7 @@ const productos = [
     {
         id: "pantalon-4",
         titulo: "Pantalón 4",
-        imagen: "../../img/pantalones/pantalon4.jpg" ,
+        imagen: "./img/pantalones/pantalon4.jpg" ,
         precio: 6500 ,
         categoria : {
             nombre: "Pantalones",
@@ -142,7 +142,7 @@ const productos = [
     {
         id: "pantalon-5",
         titulo: "Pantalón 5",
-        imagen: "../../img/pantalones/pantalon5.jpg" ,
+        imagen: "./img/pantalones/pantalon5.jpg" ,
         precio: 4500 ,
         categoria : {
             nombre: "Pantalones",
@@ -152,7 +152,7 @@ const productos = [
     {
         id: "pantalon-6",
         titulo: "Pantalón 6",
-        imagen: "../../img/pantalones/pantalon6.jpg" ,
+        imagen: "./img/pantalones/pantalon6.jpg" ,
         precio: 9000 ,
         categoria : {
             nombre: "Pantalones",
@@ -162,7 +162,7 @@ const productos = [
     {
         id: "pantalon-7",
         titulo: "Pantalón 7",
-        imagen: "../../img/pantalones/pantalon7.jpg" ,
+        imagen: "./img/pantalones/pantalon7.jpg" ,
         precio: 9000 ,
         categoria : {
             nombre: "Pantalones",
@@ -172,7 +172,7 @@ const productos = [
     {
         id: "pantalon-8",
         titulo: "Pantalón 8",
-        imagen: "../../img/pantalones/pantalon8.jpg" ,
+        imagen: "./img/pantalones/pantalon8.jpg" ,
         precio: 9000 ,
         categoria : {
             nombre: "Pantalones",
@@ -182,7 +182,7 @@ const productos = [
     {
         id: "abrigo-1",
         titulo: "Abrigo 1",
-        imagen: "../../img/abrigos/abrigo1.jpg" ,
+        imagen: "./img/abrigos/abrigo1.jpg" ,
         precio: 15000 ,
         categoria : {
             nombre: "Abrigos",
@@ -192,7 +192,7 @@ const productos = [
     {
         id: "abrigo-2",
         titulo: "Abrigo 2",
-        imagen: "../../img/abrigos/abrigo2.jpg" ,
+        imagen: "./img/abrigos/abrigo2.jpg" ,
         precio: 10000 ,
         categoria : {
             nombre: "Abrigos",
@@ -202,7 +202,7 @@ const productos = [
     {
         id: "abrigo-3",
         titulo: "Abrigo 3",
-        imagen: "../../img/abrigos/abrigo3.jpg" ,
+        imagen: "./img/abrigos/abrigo3.jpg" ,
         precio: 10000 ,
         categoria : {
             nombre: "Abrigos",
@@ -212,7 +212,7 @@ const productos = [
     {
         id: "abrigo-4",
         titulo: "Abrigo 4",
-        imagen: "../../img/abrigos/abrigo4.jpg" ,
+        imagen: "./img/abrigos/abrigo4.jpg" ,
         precio: 12000 ,
         categoria : {
             nombre: "Abrigos",
@@ -222,7 +222,7 @@ const productos = [
     {
         id: "abrigo-5",
         titulo: "Abrigo 5",
-        imagen: "../../img/abrigos/abrigo5.jpg" ,
+        imagen: "./img/abrigos/abrigo5.jpg" ,
         precio: 10000 ,
         categoria : {
             nombre: "Abrigos",
@@ -232,7 +232,7 @@ const productos = [
     {
         id: "abrigo-6",
         titulo: "Abrigo 6",
-        imagen: "../../img/abrigos/abrigo6.jpg" ,
+        imagen: "./img/abrigos/abrigo6.jpg" ,
         precio: 11000 ,
         categoria : {
             nombre: "Abrigos",
